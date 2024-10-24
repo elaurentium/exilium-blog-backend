aa51b035c45e41ac406b756de99ce95854cf97d9
elaurentium <credopuke@icloud.com>
2024-10-24 11:29:36 -0300

    feat: error handling

---
+++ src/configuration/Errors/rest_err.go
@@ -0,0 +1,69 @@
+package rest_err
+
+import "net/http"
+
+type RestErr struct {
+	Message string `JSON:"message"`
+	Error string `JSON:"error"`
+	Code int16 `JSON:"code"`
+	Causes []Causes `JSON:"causes"`
+}
+
+type Causes struct {
+	Field string `JSON:"field"`
+	Message string `JSON:"message"`
+}
+
+func (r *RestErr) Err() string {
+	return r.Message
+}
+
+func NewRestErr(message, error string, code int16, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Error: error,
+		Code: code,
+		Causes: causes,
+	}
+}
+
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Error: "Bad Request",
+		Code: http.StatusBadRequest,
+	}
+}
+
+func NewBadRequestValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Error: "Bad Request",
+		Code: http.StatusBadRequest,
+		Causes: causes,
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Error: "Internal Server Error",
+		Code: http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Error: "Not Found",
+		Code: http.StatusNotFound,
+	}
+}
+
+func NewForbiddenError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Error: "Forbidden",
+		Code: http.StatusForbidden,
+	}
+}
